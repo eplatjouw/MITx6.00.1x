4ac2bfd6fb1df10180d1248a6b1c41e8d7891613
Emiel <emiel.platjouw@gmail.com>
2019-08-14 12:11:37 +0100

    Problem 3 bug solved

--- Problem.set.3.py
+++ Problem.set.3.py
@@ -6,24 +6,25 @@ Created on Tue Aug 13 18:13:08 2019
 """
 
 s = 'azcbobobegghakl'
-r=len(s)
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 x = ""
-temp = ""
-while r > 0:
-    s = s[len(temp):]
+
+while len(s) > 0:
+    temp = ""
     print(s)
     count = 0
-    temp = ""
     for i in range(len(alphabet)):
         if s[count] == alphabet[i]:
             temp = temp + str(s[count])
             count += 1
             print(temp)
+            if s[len(s)-1] == alphabet [i]:
+                break
     if len(temp) > len(x):
         x = temp
     if x[len(x)-1] == temp[0]:
         x = x + temp
+    s = s[len(temp):]
 print("Longest substring in alphabetical order is: "+ x)            
             
    
